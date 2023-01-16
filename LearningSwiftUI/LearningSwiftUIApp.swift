c5891c366c74ac0f4b553f642ed565ddc5a7ee71
Karamjit Singh <karamjitsingh1989@gmail.com>
2023-01-16 19:48:35 +0530

    First Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by Harsimrat on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
